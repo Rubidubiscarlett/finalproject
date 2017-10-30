8f6af4370f98b041dc43f33bc728088250186ab6
Rubidubiscarlett <rubidubiscarlett@gmail.com>
2017-10-30 11:53:09 -0600

    deshonrra, desgracia

---
+++ caesar_cipher/js/app.js
@@ -0,0 +1,6 @@
+var originalMessage = prompt("Ingresa texto a codificar");
+originalMessage= originalMessage.toLowerCase();
+console.log(originalMessage);
+
+var vocabulary= ("abcdefghijklmnopqrstuvwxyz");
+vocabulary= vocabulary.split('');
